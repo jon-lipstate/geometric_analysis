22ede97ec2e3ecd8c5a4de488053547558c8d00d
jon lipstate <jon@lipstate.com>
2021-03-25 22:54:20 -0700

    jarvis march and graham scan hulls

--- src/convex_hull.rs
+++ src/convex_hull.rs
@@ -4,9 +4,12 @@ use std::cmp::Ordering;
 //https://www.youtube.com/watch?v=vPDPE66nhlo
 
 //Sample Main Code:
+// use nalgebra::Vector2;
 // mod convex_hull;
-// use convex_hull::graham_scan;
-// let mut points: Vec<Vector2<f32>> = vec![
+// use convex_hull::{graham_scan, jarvis_march};
+
+// fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     let mut points: Vec<Vector2<f32>> = vec![
 //         Vector2::new(0f32, 0f32),
 //         Vector2::new(10f32, 0f32),
 //         Vector2::new(10f32, 10f32),
@@ -15,21 +18,41 @@ use std::cmp::Ordering;
 //         Vector2::new(1f32, 0f32),
 //         Vector2::new(3f32, 3f32),
 //     ];
-//     let hull = graham_scan(&mut points);
-//     println!("Hull Length: {}", hull.len());
+
+//     let hull = graham_scan(&mut points.clone());
+//     println!("graham_scan, Hull Length: {}", hull.len());
 //     let vv: Vec<(f32, f32)> = hull.iter().map(|x: &Vector2<f32>| (x.x, x.y)).collect();
 //     println!("Hull: {:?}", vv);
 
-fn get_orientation(p: &Vector2<f32>, q: &Vector2<f32>, r: &Vector2<f32>) -> Orientation {
-    let det: f32 = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+//     let hull = jarvis_march(&mut points);
+//     println!("jarvis_march, Hull Length: {}", hull.len());
+//     let vv: Vec<(f32, f32)> = hull.iter().map(|x: &Vector2<f32>| (x.x, x.y)).collect();
+//     println!("Hull: {:?}", vv);
+//     Ok(())
+// }
+
+fn cmp_f32(a: f32, b: f32) -> Ordering {
     let threshold = 2f32 * f32::EPSILON;
-    let is_zero = det + threshold > 0f32 && det - threshold < 0f32;
+    let diff = b - a;
+    let is_zero = diff + threshold > 0f32 && diff - threshold < 0f32;
     if is_zero {
+        return Ordering::Equal;
+    } else if diff > 0f32 {
+        return Ordering::Greater;
+    } else {
+        return Ordering::Less;
+    }
+}
+
+fn get_orientation(a: &Vector2<f32>, b: &Vector2<f32>, c: &Vector2<f32>) -> Orientation {
+    let det: f32 = (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x);
+
+    if cmp_f32(0f32, det) == Ordering::Equal {
         return Orientation::Collinear;
     } else if det > 0f32 {
-        return Orientation::Clockwise;
-    } else {
         return Orientation::CounterClockwise;
+    } else {
+        return Orientation::Clockwise;
     }
 }
 #[derive(PartialEq, Debug)]
@@ -39,14 +62,34 @@ enum Orientation {
     CounterClockwise,
 }
 
-fn polar_angle(p: &Vector2<f32>, anchor: &Vector2<f32>) -> f32 {
-    //q is anchor
-    let x_span = p.x - anchor.x;
-    let y_span = p.y - anchor.y;
-    f32::atan2(y_span, x_span)
+fn sort_polar(anchor: &Vector2<f32>, a: &Vector2<f32>, b: &Vector2<f32>) -> Ordering {
+    let angle_a = f32::atan2(a.x - anchor.x, a.y - anchor.y);
+    let angle_b = f32::atan2(b.x - anchor.x, b.y - anchor.y);
+
+    match cmp_f32(angle_a, angle_b) {
+        Ordering::Greater => Ordering::Greater,
+        Ordering::Less => Ordering::Less,
+        Ordering::Equal => {
+            //Collinear: if Slope is Positive, Sort Ascending, Slope is neg or Vertical, sort Descending
+            let b_is_farther = cmp_f32(dist_squared(anchor, b), dist_squared(anchor, a));
+            let dx_a = a.x - anchor.x;
+            //Vertical Alignment | Negative Slope
+            if cmp_f32(0f32, dx_a) == Ordering::Equal || dx_a < 0f32 {
+                b_is_farther.reverse()
+            } else {
+                b_is_farther
+            }
+        }
+    }
 }
 
-pub fn graham_scan(points: &mut Vec<Vector2<f32>>) -> Vec<Vector2<f32>> {
+fn dist_squared(a: &Vector2<f32>, b: &Vector2<f32>) -> f32 {
+    let dx = b.x - a.x;
+    let dy = b.y - a.y;
+    return dx * dx + dy * dy;
+}
+
+fn set_bottom_left_point(points: &mut Vec<Vector2<f32>>) {
     //Find Min Y Point & Set as Anchor:
     let mut y_min = points[0].y;
     let mut min_index = 0;
@@ -57,35 +100,34 @@ pub fn graham_scan(points: &mut Vec<Vector2<f32>>) -> Vec<Vector2<f32>> {
             min_index = i;
         }
     }
-    let anchor: Vector2<f32> = points[min_index];
     points.swap(0, min_index);
-    //Sort Relative to Anchor:
-    points.sort_by(|a: &Vector2<f32>, b: &Vector2<f32>| {
-        let angle_a = polar_angle(a, &anchor);
-        let angle_b = polar_angle(b, &anchor);
-        if angle_a == angle_b {
-            let da = f32::powi(anchor.x - a.x, 2) + f32::powi(anchor.y - a.y, 2);
-            let db = f32::powi(anchor.x - b.x, 2) + f32::powi(anchor.y - b.y, 2);
-            if da > db {
-                return Ordering::Greater;
-            } else {
-                return Ordering::Less;
-            }
-        } else if angle_a > angle_b {
-            return Ordering::Greater;
-        } else {
-            return Ordering::Less;
-        }
-    });
-    //Perform the Scan:
-    let mut hull: Vec<Vector2<f32>> = vec![anchor, points[1]];
-    for i in 2..points.len() {
-        loop {
-            let last: &Vector2<f32> = &hull[hull.len() - 1];
-            let next_last: &Vector2<f32> = &hull[hull.len() - 2];
-            let next: &Vector2<f32> = &points[i];
+}
 
-            if get_orientation(next_last, last, next) == Orientation::CounterClockwise {
+///O(nlogn)
+//https://youtu.be/B2AJoQSZf4M
+pub fn graham_scan(points: &mut Vec<Vector2<f32>>) -> Vec<Vector2<f32>> {
+    //1. Pick Bottom Left Point (anchor)
+    //2. Sort all other points relative (by angle) to anchor
+    //2a. X-Product - save cpu cycles, VxW -> +:CCW, -:CW, 0:||
+    //3. Push Points onto a stack, if CCW, continue, if CW, pop until CCW
+    set_bottom_left_point(points);
+    let anchor: Vector2<f32> = points[0];
+    points.remove(0);
+    //Sort Relative to Anchor:
+    points.sort_by(|a: &Vector2<f32>, b: &Vector2<f32>| sort_polar(&anchor, a, b));
+    let vv: Vec<(f32, f32)> = points.iter().map(|x: &Vector2<f32>| (x.x, x.y)).collect();
+    println!("sorted points: {:?}", vv);
+    //Perform the Scan:
+    let mut hull: Vec<Vector2<f32>> = vec![anchor, points[0]];
+    for i in 1..points.len() {
+        loop {
+            let hull_last: &Vector2<f32> = &hull[hull.len() - 1];
+            let hull_second_last: &Vector2<f32> = &hull[hull.len() - 2];
+            let points_next: &Vector2<f32> = &points[i];
+
+            if get_orientation(hull_second_last, hull_last, points_next)
+                == Orientation::CounterClockwise
+            {
                 break;
             }
             hull.remove(hull.len() - 1);
@@ -96,6 +138,48 @@ pub fn graham_scan(points: &mut Vec<Vector2<f32>>) -> Vec<Vector2<f32>> {
         } //end loop
         hull.push(points[i]);
     } // end for
+
+    return hull;
+}
+
+///O(nh)
+//https://youtu.be/gfZUkFf3kVA
+pub fn jarvis_march(points: &mut Vec<Vector2<f32>>) -> Vec<Vector2<f32>> {
+    set_bottom_left_point(points);
+    let mut hull: Vec<Vector2<f32>> = vec![points[0]];
+    let mut prev: Vector2<f32> = points[0];
+    loop {
+        let mut candidate: Option<Vector2<f32>> = None;
+        for i in 0..points.len() {
+            let next: Vector2<f32> = points[i];
+            if next == prev {
+                continue;
+            }
+            if candidate.is_none() {
+                candidate = Some(next);
+                continue;
+            }
+            match get_orientation(&prev, &candidate.unwrap(), &next) {
+                Orientation::Collinear => {
+                    let candidate_is_closer =
+                        dist_squared(&prev, &candidate.unwrap()) < dist_squared(&prev, &next);
+                    if candidate_is_closer {
+                        candidate = Some(next);
+                    }
+                }
+                Orientation::Clockwise => candidate = Some(next),
+                Orientation::CounterClockwise => (),
+            }
+        } // end for
+        if candidate.is_some() {
+            if candidate.unwrap() == hull[0] {
+                //returned to start
+                break;
+            }
+            hull.push(candidate.unwrap());
+            prev = candidate.unwrap();
+        }
+    }
 
     return hull;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 #![allow(dead_code, unused_imports)]
 use nalgebra::Vector2;
+mod convex_hull;
+use convex_hull::{graham_scan, jarvis_march};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut points: Vec<Vector2<f32>> = vec![
+        Vector2::new(0f32, 0f32),
+        Vector2::new(10f32, 0f32),
+        Vector2::new(10f32, 10f32),
+        Vector2::new(0f32, 10f32),
+        Vector2::new(3f32, 0f32),
+        Vector2::new(1f32, 0f32),
+        Vector2::new(3f32, 3f32),
+    ];
+
+    let hull = graham_scan(&mut points.clone());
+    println!("graham_scan, Hull Length: {}", hull.len());
+    let vv: Vec<(f32, f32)> = hull.iter().map(|x: &Vector2<f32>| (x.x, x.y)).collect();
+    println!("Hull: {:?}", vv);
+
+    let hull = jarvis_march(&mut points);
+    println!("jarvis_march, Hull Length: {}", hull.len());
+    let vv: Vec<(f32, f32)> = hull.iter().map(|x: &Vector2<f32>| (x.x, x.y)).collect();
+    println!("Hull: {:?}", vv);
     Ok(())
 }
 //concave hull:
